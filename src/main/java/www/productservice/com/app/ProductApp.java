1befe6e3010b0c40d52dc1487fceaa1db1867169
Aman Woldtensae <woldtensae.aman@gmail.com>
2019-04-17 17:52:06 -0400

    adding product service

--- src/main/java/www/productservice/com/app/ProductApp.java
+++ src/main/java/www/productservice/com/app/ProductApp.java
@@ -2,8 +2,16 @@ package www.productservice.com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(value= {"www.productservice.com.controller", 
+		"www.productservice.com.service", "www.productservices.com.domain", 
+		"www.productservice.com.repository"})
+@EntityScan("www.productservices.com.domain")
+@EnableJpaRepositories("www.productservice.com.repository")
 public class ProductApp {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApp.class, args);
--- src/main/java/www/productservices/com/domain/Product.java
+++ src/main/java/www/productservices/com/domain/Product.java
@@ -6,17 +6,19 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
 public class Product {
+	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	@Column(name="product_name")
 	private String productName;
 	
-	@ManyToOne(fetch= FetchType.LAZY)
+	@ManyToOne(fetch= FetchType.EAGER)
 	@JoinColumn(name ="category_id")
 	private Category category;
 
